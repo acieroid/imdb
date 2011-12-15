6628533a5b29db2a2ead495a4f5e8ac85ed8e29d
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-12-15 23:11:40 +0100

    adding sort to search

--- site/pages/search.py
+++ site/pages/search.py
@@ -11,18 +11,28 @@ from pages import BasePage
 tokens = (
     'OPERATOR',
     'COMPARATOR',
+    'CONTAINER',
+    'CONTAINER_OP',
     'IDENTIFIER',
+    'SORT',
     'VALUE',
     'LPAREN',
     'RPAREN',
-    'SPACE'
+    'LBRACE',
+    'RBRACE',
+    'SPACE',
 )
 
 precedence = (
+    ('left', 'SORT'),
+    ('left', 'CONTAINER_OP'),
     ('left', 'OPERATOR'),
 )
 
 t_OPERATOR = r'(and|or)'
+t_CONTAINER_OP = r'(;|,)'
+t_CONTAINER = r'contains'
+t_SORT = r'sort(\+|\-)'
 t_COMPARATOR = r'(>=|<=|<|>|==|=~)'
 t_IDENTIFIER = r'[A-Z][a-zA-Z_]+'
 def t_VALUE(t):
@@ -32,14 +42,24 @@ def t_VALUE(t):
     else:
         t.value = int(t.value)
     return t
-t_LPAREN = '\('
-t_RPAREN = '\)'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
 t_ignore_SPACE = r'\s+'
 
 def t_error(t):
     raise TypeError('error when lexing "%s"' % t.value)
 
 ply.lex.lex()
+
+def p_query_sort(p):
+    'query : condition SORT IDENTIFIER'
+    p[0] = [p[2], p[3], p[1]]
+
+def p_query(p):
+    'query : condition'
+    p[0] = p[1]
 
 def p_paren(p):
     'condition : LPAREN condition RPAREN'
@@ -52,6 +72,22 @@ def p_op(p):
 def p_comp(p):
     'condition : IDENTIFIER COMPARATOR VALUE'
     p[0] = [p[2], p[1], p[3]]
+
+def p_cont(p):
+    'condition : IDENTIFIER CONTAINER container'
+    p[0] = [p[2], p[1], p[3]]
+
+def p_container_op(p):
+    'container : LBRACE container_content RBRACE'
+    p[0] = p[2]
+
+def p_container_content_op(p):
+    'container_content : condition CONTAINER_OP condition'
+    p[0] = [p[2], p[1], p[3]]
+
+def p_container_content(p):
+    'container_content : condition'
+    p[0] = p[1]
 
 def p_error(p):
     raise TypeError('syntax error at "%s"' % p.value)
@@ -75,6 +111,12 @@ def convert_pattern_matching(s):
     # TODO: what to do if the user has entered a query with '%' and '?'
     return re.sub(r'\?', '_', re.sub(r'\*', '%', s))
 
+class QueryError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
 def convert(l):
     conversion_table = {
         'and': ' and ',
@@ -95,9 +137,19 @@ def convert(l):
     def simple_identifier(x):
         return in_list(x, ['Title', 'Year', 'Note', 'EndYear',
                            'Season', 'EpisodeNum', 'EpisodeTitle',
-                           'FirstName', 'LastName'])
+                           'FirstName', 'LastName', 'Num'])
+    def valid_var(t, x):
+        if t == 'Episode':
+            return in_list(x, ['Title', 'Year', 'Note', 'EndYear', 
+                               'Season', 'EpisodeNum', 'EpisodeTitle'])
+        elif t == 'Serie':
+            return in_list(x, ['Title', 'Year', 'Note', 'EndYear'])
+        elif t == 'Movie':
+            return in_list(x, ['Title', 'Year', 'Note'])
+        elif t == 'Person':
+            return in_list(x, ['FirstName', 'LastName', 'Num'])
     def combine(a, op, b):
-        (res_a , params_a) = a
+        (res_a, params_a) = a
         (res_b, params_b) = b
         return (res_a + op + res_b, params_a + params_b)
     def helper(l):
@@ -113,31 +165,60 @@ def convert(l):
                 elif l[1] == 'Genre' or l[1] == 'Country' or l[1] == 'Language':
                     return (' exists (select 1 from %s where %s.ID = W.ID and %s.%s = ?)' %
                             ((l[1],)*4), [l[2]])
+                else:
+                    raise QueryError('Unknown identifier: %s' % l[1])
+            else:
+                raise QueryError('Unknown operator: %s' % l[0])
         else:
             return ('', [])
+
+    sort = False
+    if re.match(t_SORT, l[0]):
+        sort = True
+        sort_op = l[0]
+        sort_var = l[1]
+        l = l[2]
+
     if satisfies(l, lambda x: (x == 'EpisodeNum' or
                                x == 'EpisodeTitle')):
-        if satisfies(l, lambda x: (x == 'FirstName' or x == 'LastName')):
-            raise TypeError('Episode cannot have FirstName or LastName')
-        return (combine(('select ID from Work W, Serie, Episode where W.ID = Serie.ID and W.ID = Episode.ID and ',
-                         []), '', helper(l)),
-                'Episode')
+        if satisfies(l, lambda x: not valid_var('Episode')):
+            raise QueryError('Query contains invalid variable for episode')
+        res = combine(('select ID from Work W, Serie, Episode where W.ID = Serie.ID and W.ID = Episode.ID and ',
+                        []), '', helper(l))
+        t = 'Episode'
     elif satisfies(l, lambda x: (x == 'EndYear')):
-        if satisfies(l, lambda x: (x == 'FirstName' or x == 'LastName')):
-            raise TypeError('Serie cannot have FirstName or LastName')
-        return (combine(('select ID from Work W, Serie where W.ID = Serie.ID and ',
-                         []), '', helper(l)),
-                'Serie')
+        if satisfies(l, lambda x: not valid_var('Serie')):
+            raise QueryError('Query contains invalid variable for serie')
+        res = combine(('select ID from Work W, Serie where W.ID = Serie.ID and ',
+                        []), '', helper(l))
+        t = 'Serie'
     elif satisfies(l, lambda x: (x == 'FirstName' or
                                  x == 'LastName')):
-        if satisfies(l, lambda x: (x != 'FirstName' and x != 'LastName')):
-            raise TypeError('Person can only have FirstName and LastName')
-        return (combine(('select FirstName, LastName, Num from Person where ',
-                        []), '', helper(l)),
-                'Person')
+        if satisfies(l, lambda x: not valid_var('Person', x)):
+            raise QueryError('Query contains invalid variable for person')
+        res = combine(('select FirstName, LastName, Num from Person where ',
+                        []), '', helper(l))
+        t = 'Person'
     else:
-        return (combine(('select ID from Work W where ', []), '', helper(l)),
-                'Movie')
+        if satisfies(l, lambda x: not valid_var('Movie', x)):
+            raise QueryError('Query contains invalid variable for movie')
+        res = combine(('select ID from Work W where ', []), '', helper(l))
+        t = 'Movie'
+
+    if sort:
+        if simple_identifier(sort_var):
+            if valid_var(t, sort_var):
+                sort = ' order by %s' % sort_var
+                if sort_op == 'sort-':
+                    sort += ' desc'
+                l = l[2]
+            else:
+                raise QueryError('Invalid sort variable: %s' % sort_var)
+        else:
+            raise QueryError('Cannot sort by %s' % sort_var)
+        res = (res[0] + sort, res[1])
+
+    return (res, t)
 
 # Page
 class Search(BasePage):
@@ -150,6 +231,10 @@ class Search(BasePage):
         # parse the query
         try:
             parsed = ply.yacc.parse(self.get_argument('search', ''))
+        except TypeError as (e,):
+            self.error('Your query was incorrect: %s' % e)
+
+        try:
             ((query, args), typeofdata) = convert(parsed)
             
             # execute the query
@@ -160,6 +245,5 @@ class Search(BasePage):
             
             self.write(loader.load('search.html').generate(results=results,
                                                            typeofdata=typeofdata))
-        except TypeError as (err,):
-            self.error('Your query was incorrect: %s' % err)
-
+        except QueryError as e:
+            self.error('Your query was incorrect: %s' % e.message)
